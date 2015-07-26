cc8592a8115f082f09e19c386235a9c31be95cec
babak <b.dastmalchi@gmail.com>
2015-07-26 16:39:55 -0500

    5th commit

--- ui.R
+++ ui.R
@@ -6,14 +6,13 @@ shinyUI(pageWithSidebar(
 # Application title
 headerPanel("Approximate stopping distance of a car vs its speed"),
 
-# Sidebar with a slider input for number of observations
 sidebarPanel(
 numericInput("degree", "Polynomial fit degree", 2, 1, 4, 1),
 sliderInput("speed", "Speed [mph]:", min = 4, max = 25, value = 10),
 h4("Predicted distance [ft]:"),
 verbatimTextOutput("y")
 ),
-# Show a plot of the generated distribution
+
 mainPanel(
   tabsetPanel(
     tabPanel("Plot", plotOutput("distPlot")), 
